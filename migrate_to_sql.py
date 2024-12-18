651e5b6d3dd25dc28d0e7de49cd2628f6ca3969c
°peps° <peps@peps>
2024-12-18 13:16:38 +0100

    secondo script completo check d consistenza inserito

--- migrate_to_sql.py
+++ migrate_to_sql.py
@@ -6,46 +6,15 @@ import pandas as pd
 import argparse
 import importlib.util
 
-#directory 
-directory = "generated_data"
+#log directory
 log_directory="migration_log"
 log_file = os.path.join(log_directory,"migration.log")
 
 default_db_name = "migration.db"
-
+default_table_name = "testpw"
 LOG_FILE_NOT_FOUND = "log_file_not_found"
 FILE_NOT_MIGRATED ="file_not_migrated"
 FILE_MIGRATED= "file_migrated"
-
-
-
-def check_requirements():
-    
-    requiremetntsNotInstalled = []
-    try:
-        with open("./requirement.txt") as f:
-            requirements = f.readlines()
-
-
-        for requirement in requirements:
-            
-            try:
-                importlib.util.find_spec(requirement.strip())
-            except ModuleNotFoundError:
-                print(f"Requirement {requirement} not found")
-                requiremetntsNotInstalled.append(requirement)          
-        
-        if requiremetntsNotInstalled:
-            print(f"Please install the following requirements: {requiremetntsNotInstalled}")
-            return False
-        else:
-            print("All requirements are installed")
-            return True
-        
-    except Exception as e:
-        print(f"Error during requirement check: {e}")
-        return False
-
 
 
 
@@ -70,10 +39,10 @@ def initialize_logger():
 
 
 #Write log file
-def write_log(fileName):
+def write_log(fileName,table_name):    
     try:
         with open(log_file,'a') as log:
-            log.write(f"{fileName}\n")                
+            log.write(f"{fileName} in to table {table_name}\n")                
     
     except Exception as e:
         print(f"Error during log writing: {e}")
@@ -102,20 +71,29 @@ def check_exist_migration_file(file_path):
 
 
 #retunr status of migration file 
-def migration_status(file_name, log_file):
+def migration_status(table_name,file_name, log_file):
    statusMap ={}
-      
+   if table_name is None:
+         table_name = default_table_name
+         print(f"Table name not provided, using default table name {default_table_name}")
+   
+   
    if os.path.exists(log_file):
        with open(log_file,'r') as log:
            fileMigrated = {line.strip() for line in log.readlines()}           
-                 
+             
            if file_name in fileMigrated:
-               statusMap[file_name]= FILE_MIGRATED
+               tableMigrated=file_name.split(" in to table ")[-1].strip() #mi serve nel caso in cui si vuole migrare un file già migrato in un'altra tabella
+               if tableMigrated==table_name:
+                   statusMap[file_name]=FILE_NOT_MIGRATED
+               else:
+                   statusMap[file_name]= FILE_MIGRATED
            else:
                statusMap[file_name]=FILE_NOT_MIGRATED      
           
    else:       
-           statusMap[file_name]=LOG_FILE_NOT_FOUND
+           statusMap[file_name]=LOG_FILE_NOT_FOUND       
+           
     
    return statusMap
 
@@ -136,7 +114,7 @@ def migrate_to_db(file,db_name,table_name):
         db_name = default_db_name
         print(f"Database name not provided, using default database {default_db_name}")
     if table_name is None:
-        table_name = 'testpw'
+        table_name = default_table_name
         print(f"Table name not provided, using default table name {table_name}")
 
 
@@ -150,7 +128,7 @@ def migrate_to_db(file,db_name,table_name):
             
             create_table_on_db(c,table_name,data)       
             insert_data_on_db(conn,c,table_name,data)
-            write_log(filename)
+            write_log(filename,table_name)
   
     except Exception as e:
         print(f"Error during data migration: {e}")
@@ -165,7 +143,7 @@ def create_table_on_db(c,table_name,data):
     try:
         columns= "id INTEGER PRIMARY KEY AUTOINCREMENT, "
         columns += ", ".join([f"{col.replace(' ','_').lower()} TEXT" for col in data.columns])
-        print(f"Columns: {columns}")       
+             
         create_table = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns});"
         c.execute(create_table)
         print(f"Succesfully created SQL table {table_name}")
@@ -181,15 +159,13 @@ def create_table_on_db(c,table_name,data):
 def insert_data_on_db (conn,c,table_name,data):
     print(f"Insert data into SQL table {table_name}...")
     try:
-        conn.execute("BEGIN TRANSACTION;")       
-
+        conn.execute("BEGIN TRANSACTION;")
 
         columns = ", ".join([col.replace(' ', '_').lower() for col in data.columns])
         
         for row in data.itertuples(index=False,name=None):
             placeholders = ", ".join("?"*len(row))
-            insert=f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"
-            print(f"Inserting data: {insert}")
+            insert=f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"            
             c.execute(insert,row)           
         conn.commit()
         print(f"Successfully inserted data into SQL table {table_name}")
@@ -218,12 +194,26 @@ def migration_phase(file,statusMap,db_name,table_name):
 
 
 def get_data_migrated(db_name,table_name):
+    
+    if db_name is None:
+        db_name = default_db_name
+        print(f"Database name not provided, using default database {default_db_name}")
+        
     try:
-            with sqlite3.connect(db_name) as conn:
-                c = conn.cursor()
-                c.execute(f"SELECT * FROM {table_name};")
-                data = c.fetchall()
-                return data
+        with sqlite3.connect(db_name) as conn:                
+            c = conn.cursor()
+                
+            columns = c.execute(f"PRAGMA table_info({table_name});")
+            columns = [col[1] for col in columns]
+            columns_select = [col for col in columns if col != 'id']            
+                
+                
+            c.execute(f"SELECT {', '.join(columns_select)} FROM {table_name} ORDER BY id ASC;")
+            data = c.fetchall()
+             #normalizzazione dati per confronto
+            convert_data = [{columns_select[i]:row[i] for i in range(len(row))} for row in data]
+                
+            return convert_data
     except Exception as e:
         print(f"Error during file name extraction: {e}")
         return []
@@ -231,7 +221,9 @@ def get_data_migrated(db_name,table_name):
 def get_data_from_file(file):
     try:
         data= pd.read_excel(file)
-        return data
+        #normalizzazion dati per confronto
+        convert_data = data.to_dict(orient='records')
+        return convert_data
     except Exception as e:
         print(f"Error during data extraction: {e}")
         return []    
@@ -257,6 +249,11 @@ def get_name_of_table_on_DB(db_name):
 
 
 def consistency_check(file,db_name,table_name):
+    
+    if table_name is None:
+        table_name = default_table_name
+        print(f"Table name not provided, using default table name {default_table_name}")
+    
     print(f"Consistency check in progress...")
     tablesName = get_name_of_table_on_DB(db_name)
     print(f"Tables in database: {tablesName}")     
@@ -268,76 +265,76 @@ def consistency_check(file,db_name,table_name):
         print(f"Table {table_name} found in database")
     
     data_from_file = get_data_from_file(file)
-    data_from_db = get_data_migrated(db_name,table_name)
+    data_from_db = get_data_migrated(db_name,table_name)    
     
-    converted_data_from_db = pd.DataFrame(data_from_db)
-    converted_data_from_db = converted_data_from_db.iloc[:,:-1]
     
-    converted_data_from_db.columns = data_from_file.columns
+    missing_data = []
     
-
+    for row in data_from_file:        
+        if row not in data_from_db:
+                missing_data.append(row)       
+       
     
-    #tobecompleteeeeeesss....
-    if data_from_file.equals(converted_data_from_db):
+    if not missing_data:
         print(f"Data from file and data from database are consistent")
     else:
         print(f"Data from file and data from database are not consistent")
-        missing_data = data_from_file[~data_from_file.isin(converted_data_from_db).all(axis=1)]
         print(f"Missing data: {missing_data}")
+        
+         
+    
    
 
      
         
 #Main function
 def main():
-    if not check_requirements():
-        print("Requirements not installed, please install requirements")
-        return
-
     parser = argparse.ArgumentParser(description="Migrate data from Excel to SQL")
 
-
     parser.add_argument("-f","--file",help="Path to the file .xlsx to be migrated")
-    parser.add_argument("-t","--table",help="Name of the table to be created or update in the database")
-    parser.add_argument("-d","--database",help="Path to the SQLite database file")
-
-
-    args = parser.parse_args()
-        
+    parser.add_argument("-t","--table",help="Name of the table to be created on the database")
+    args = parser.parse_args()        
     
     #Initialize logger
     initialize_logger()
 
-
     file_to_be_migrate,file_path = check_exist_migration_file(args.file)
-
 
     if file_to_be_migrate is not None and file_path is not None:
         print("Ready for migration..\n")
         print(f"File to be migrated: {file_to_be_migrate}")
         print(f"File path: {file_path}")
         print(f"Log file: {log_file}")
-        print(f"Database name : {args.database} if database is none, default database will be used")
-        print(f"Table name : {args.table} if table name args is none, default table name will be used\n")
-
+        print(f"Database name : {default_db_name}")
+        if args.table is not None:
+            print(f"Table name : {args.table}")
+        else:            
+            print(f"Table name is not provided, using default table name {default_table_name}\n")
 
         tobemigrate = input("Are you sure you want to proceed? (y/n): ")
             
         if tobemigrate == "y":
-            status_map = migration_status(file_to_be_migrate,log_file)
-            migration_phase(file_path,status_map, args.database, args.table)    
+            status_map = migration_status(args.table,file_to_be_migrate,log_file)
+            migration_phase(file_path,status_map, None, args.table)    
             print("Migration completed")
             tobeConsistencyCheck = input("Do you want to perform a consistency check? (y/n): ")
             if tobeConsistencyCheck == "y":
-                consistency_check(file_path,args.database,args.table)
+                consistency_check(file_path,None,args.table)
                 sys.exit(0)
             else:
                 print("Consistency check aborted")
+                sys.exit(0)
                     
-        else:
-            print("Migration aborted")
+        elif tobemigrate == "n":
+                print("Migration aborted")
+                sys.exit(0)
+        else :
+                print("Invalid input\n")
+                print("Migration aborted")
+                sys.exit(0)
     else:
         print("No file to be migrated")
+        sys.exit(0)
 
     
 
